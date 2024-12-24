9d31326aea4f897cb93e80ff54e956406c04ddb8
Mohamed-Abdelrazek-Mohamed <mabdelrazek@hindam.sa>
2024-12-24 22:23:07 +0200

    renaming

--- upload_gateway.tf
+++ upload_gateway.tf
@@ -34,7 +34,7 @@ resource "aws_api_gateway_integration" "upload-api" {
   rest_api_id             = aws_api_gateway_rest_api.upload-api.id
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.my_lambda.invoke_arn
+  uri                     = aws_lambda_function.upload_lambda.invoke_arn
   content_handling        = "CONVERT_TO_BINARY"
 }
 
--- upload_lambda.tf
+++ upload_lambda.tf
@@ -1,12 +1,12 @@
 # Create Lambda function
-resource "aws_lambda_function" "my_lambda" {
-  function_name    = "my_lambda"
+resource "aws_lambda_function" "upload_lambda" {
+  function_name    = "upload_lambda"
   handler          = "bootstrap"
   runtime          = "provided.al2"
   role             = aws_iam_role.upload_lambda_role.arn
-  filename         = "lambda.zip"
+  filename         = "upload-image.zip"
   architectures    = ["arm64"]
-  source_code_hash = filebase64sha256("lambda.zip")
+  source_code_hash = filebase64sha256("upload-image.zip")
   environment {
     variables = {
       "BUCKET_NAME" : aws_s3_bucket.uploaded-images.bucket
@@ -18,7 +18,7 @@ resource "aws_lambda_function" "my_lambda" {
 resource "aws_lambda_permission" "apigw_lambda" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.my_lambda.function_name
+  function_name = aws_lambda_function.upload_lambda.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.upload-api.execution_arn}/*/*"
 }
